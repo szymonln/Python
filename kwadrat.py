40ab1fd52a283e68644e088ac8e7a9d6ead4e0b1
Szymon Lichoń <slichon@lab429-06.dydaktyka.icsr.agh.edu.pl>
2018-03-15 14:21:36 +0100

    kwadrat

--- kwadrat.py
+++ kwadrat.py
@@ -2,8 +2,33 @@
 import sys
 import math
 
-def solve(a,b,c):
+def solve(a, b, c):
     delta=b**2-4*a*c
-    if(delta>0)
-        return ((-b-sqrt(delta))/2*a, )
+    if delta > 0:
+        result = [(-b-math.sqrt(delta))/2*a]
+        result.append((-b+math.sqrt(delta))/2*a)
+        return result
+    if delta == 0:
+        result = [-b/2*a]
+        return result
+    if delta < 0:
+        return
 
+
+try:
+    a = (float)(sys.argv[1])
+except ValueError:
+    print ("Ojej, a przeciez to nie jest float...")
+    exit()
+try:
+    b = (float)(sys.argv[2])
+except ValueError:
+    print ("Ojej, a przeciez to nie jest float...")
+    exit()
+try:
+   c = (float)(sys.argv[3])
+except ValueError:
+    print ("Ojej, a przeciez to nie jest float...")
+    exit()
+
+print(solve(a,b,c))
